840bfe69e07592f55c099e77c872ad509d0fcfec
Rob Winch <rwinch@users.noreply.github.com>
2020-03-31 10:53:52 -0500

    Remove Security

--- src/main/java/com/aleks/test/TestApplication.java
+++ src/main/java/com/aleks/test/TestApplication.java
@@ -2,6 +2,9 @@ package com.aleks.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class TestApplication {
@@ -10,4 +13,8 @@ public class TestApplication {
 		SpringApplication.run(TestApplication.class, args);
 	}
 
+	@Bean
+	CorsFilter corsFilter(CorsConfigurationSource corsConfigurationSource) {
+		return new CorsFilter(corsConfigurationSource);
+	}
 }
